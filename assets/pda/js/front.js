00b1433a65882fdfe125da410e048536ac79065b
Ritwik Sharma <ritwik.1204@gmail.com>
2018-03-29 23:39:45 +0530

    update front.js

--- assets/pda/js/front.js
+++ assets/pda/js/front.js
@@ -89,15 +89,7 @@ function animations() {
  *  =======================================*/
 
 function sliding() {
-    $('.scrollTo, #navigation a').click(function (event) {
-        event.preventDefault();
-        var full_url = this.href;
-        var parts = full_url.split("#");
-        var trgt = parts[1];
-
-        $('body').scrollTo($('#' + trgt), 800, {offset: -80});
-
-    });
+ 
 }
 
 /* =========================================
